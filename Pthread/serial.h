466786b5e9108fcbe37e0709464d27eb919c00b0
Devindie Samaranayake <gdcsamaranayake2001@gmail.com>
2025-09-02 22:35:53 +0530

    serial header updated

--- Pthread/serial.h
+++ Pthread/serial.h
@@ -1,6 +1,14 @@
 #ifndef SERIAL_H
 #define SERIAL_H
 
-//Serial Linked List implementation function signature
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+#include "linked_list.h"
+#include "global.h"
+
+// Serial Linked List implementation function signature
+unsigned long serialExecution(int total_operations, int member_frac, int insert_frac, int delete_frac);
 
 #endif
