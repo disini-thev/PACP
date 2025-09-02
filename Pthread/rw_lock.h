9a872b22abef56a325c5a5f7d27becef863007c7
disini-thev <ma17disini@gmail.com>
2025-09-03 00:38:57 +0530

    rw lock implementation

--- Pthread/rw_lock.h
+++ Pthread/rw_lock.h
@@ -1,10 +1,29 @@
-#ifndef SERIAL_H
-#define SERIAL_H
+#ifndef RWLOCK_H
+#define RWLOCK_H
+
+#include <pthread.h>
+
+void * threadFuncRW(void * args);
 
 typedef struct rw_lock_data{
-    // Read-Write Lock data structure
+    pthread_rwlock_t rwlock;
+    pthread_mutex_t count_mutex;
+    list_node *head;
+
+    int m;
+    int member_frac;
+    int insert_frac;
+    int delete_frac;
+
+    int member_op_count;
+    int insert_op_count;
+    int delete_op_count;
+    int total_op_count;
+
 } rw_lock_data;
 
-// rw lock execution signature
+unsigned long readwriteExecution(int total_ops, 
+    int member_frac, int insert_frac, int delete_frac, 
+    int thread_count);
 
 #endif
