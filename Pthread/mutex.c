3ff08d29552e4df24d92983be9f64de44948299d
Devindie Samaranayake <gdcsamaranayake2001@gmail.com>
2025-09-03 01:02:54 +0530

    Updated mutex.c

--- Pthread/mutex.c
+++ Pthread/mutex.c
@@ -1,12 +1,14 @@
 #include <stdlib.h>
 #include <sys/time.h>
+#include <pthread.h>
 #include "linked_list.h"
 #include "mutex.h"
 #include "global.h"
 
-unsigned long mutexExecution(int total_operations, int mMember, int mInsert, int mDelete, int thread_count) {
+unsigned long mutexExecution(int total_operations, int mMember, int mInsert, int mDelete, int thread_count)
+{
 
-    //initialize mutex_data structure
+    // initialize mutex_data structure
     mutex_data thread_data;
     thread_data.head = NULL;
 
@@ -20,15 +22,15 @@ unsigned long mutexExecution(int total_operations, int mMember, int mInsert, int
     thread_data.deleteOp_count = 0;
     thread_data.totalOp_count = 0;
 
-    //initialize the linked list
+    // initialize the linked list
 
     int n = 1000;
     int count = 0;
 
-    while ( count < n) {
-        Insert(rand()%MAX, &thread_data.head);
+    while (count < n)
+    {
+        Insert(rand() % MAX, &thread_data.head);
         count++;
-
     }
 
     // Initialize the mutex
@@ -44,12 +46,14 @@ unsigned long mutexExecution(int total_operations, int mMember, int mInsert, int
     gettimeofday(&start, NULL);
 
     // Create threads
-    for (int thread = 0; thread < thread_count; thread++) {
-        pthread_create(&thread_handles[thread], NULL, threadFuncMutex, (void*)&thread_data);
+    for (int thread = 0; thread < thread_count; thread++)
+    {
+        pthread_create(&thread_handles[thread], NULL, threadFuncMutex, (void *)&thread_data);
     }
 
     // Join threads
-    for (int thread = 0; thread < thread_count; thread++) {
+    for (int thread = 0; thread < thread_count; thread++)
+    {
         pthread_join(thread_handles[thread], NULL);
     }
 
@@ -58,7 +62,7 @@ unsigned long mutexExecution(int total_operations, int mMember, int mInsert, int
     // End time
     gettimeofday(&end, NULL);
 
-    unsigned long exec_time = (end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec; 
+    unsigned long exec_time = (end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec;
 
     // Destroy the mutex
     pthread_mutex_destroy(&thread_data.mutex);
@@ -66,21 +70,24 @@ unsigned long mutexExecution(int total_operations, int mMember, int mInsert, int
     Destructor(thread_data.head);
     printf("Mutex run complete\n");
     return exec_time;
-
 }
 
 // Thread function for mutex implementation
-void *threadFuncMutex(void* args) {
+void *threadFuncMutex(void *args)
+{
 
-    mutex_data* thread_data = args;
+    mutex_data *thread_data = args;
 
-    while(thread_data->totalOp_count < thread_data->total_operations){
+    while (thread_data->totalOp_count < thread_data->total_operations)
+    {
 
         int rand_value = rand() % MAX;
         int operation_number = rand() % 3; // generate a random number between 0 and 2  ex:{0,1,2}
 
-        if (operation_number==0 && thread_data->insertOp_count < thread_data->mInsert){
-            if (thread_data->totalOp_count < thread_data->total_operations){
+        if (operation_number == 0 && thread_data->insertOp_count < thread_data->mInsert)
+        {
+            if (thread_data->totalOp_count < thread_data->total_operations)
+            {
                 /* Critical section */
                 pthread_mutex_lock(&thread_data->mutex);
                 Insert(rand_value, &thread_data->head);
@@ -89,8 +96,10 @@ void *threadFuncMutex(void* args) {
                 pthread_mutex_unlock(&thread_data->mutex);
             }
         }
-        else if(operation_number==1 && thread_data->deleteOp_count < thread_data->mDelete){
-            if (thread_data->totalOp_count < thread_data->total_operations){
+        else if (operation_number == 1 && thread_data->deleteOp_count < thread_data->mDelete)
+        {
+            if (thread_data->totalOp_count < thread_data->total_operations)
+            {
                 /* Critical section */
                 pthread_mutex_lock(&thread_data->mutex);
                 Delete(rand_value, &thread_data->head);
@@ -99,8 +108,10 @@ void *threadFuncMutex(void* args) {
                 pthread_mutex_unlock(&thread_data->mutex);
             }
         }
-        else if(thread_data->memberOp_count < thread_data->mMember){
-            if (thread_data->totalOp_count < thread_data->total_operations){
+        else if (thread_data->memberOp_count < thread_data->mMember)
+        {
+            if (thread_data->totalOp_count < thread_data->total_operations)
+            {
                 /* Critical section */
                 pthread_mutex_lock(&thread_data->mutex);
                 Member(rand_value, thread_data->head);
@@ -109,7 +120,6 @@ void *threadFuncMutex(void* args) {
                 pthread_mutex_unlock(&thread_data->mutex);
             }
         }
-
     }
 
     return NULL;
