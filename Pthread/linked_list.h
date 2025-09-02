6e71e3a7618b4f7f2ea3e112846aceb5c54a173c
disini-thev <ma17disini@gmail.com>
2025-09-02 11:07:23 +0530

    linked list header file added

--- Pthread/linked_list.h
+++ Pthread/linked_list.h
@@ -7,5 +7,12 @@ typedef struct list_node {
 } list_node;
 
 //linked list function signatures member, insert, delete, destructor, print
+// _p for pointer to head - for reading files, _pp for pointer to pointer to head
+int Member(int value, list_node *head_p);
+int Insert(int value, list_node **head_pp);
+int Delete(int value, list_node **head_pp);
+
+void Destructor(list_node **head_pp);
+void PrintList(list_node *head_p);
 
 #endif
